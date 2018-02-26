469a24d6337171970d7d51470285194441d58a14
Andrew Ryan <aryan17@mail.csuchico.edu>
2018-02-25 17:25:01 -0800

    table contains house data, created propertyInfo page

--- webscraper/urls.py
+++ webscraper/urls.py
@@ -4,7 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.WSdatabase, name='WSdatabase'),
+    url(r'propertyInfo',views.propertyInfo, name='propertyInfo'),
 ]
-# urlpatterns = [
-#     url(r'^$', views.index, name='index'),
-# ]
--- webscraper/views.py
+++ webscraper/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render
+# from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from .models import *
+
+# from django.db.models import Count
 
 # Create your views here.
+# def WSdatabase(request):
+#     context = {
+#         'title':"WSdatabase",
+#         }
+#     return render(request,'WSdatabase.html',context)
+
 def WSdatabase(request):
+    all_houses = House.objects.all()
+    num_of_houses = House.objects.count()
+    # num_of_houses = House.objects.annotate(Count('caseNum'))
+    # num_of_houses = House.objects.all.count()
+    # num_of_houses = House.objects.filter(streetNum='3341').count()
     context = {
+        # 'house_total':num_of_houses,
+        'house_list':all_houses,
         'title':"WSdatabase",
         }
-    return render(request,'WSdatabase.html',context)
+    return render(request,'wsdatabase.html',context, num_of_houses)
+
+def propertyInfo(request):
+    context = {
+        'title':"Property Info",
+        }
+    return render(request,'propertyInfo.html',context)

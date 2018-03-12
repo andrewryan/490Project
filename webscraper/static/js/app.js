06d6e246436b270edf33b7396d5fd3e17838221e
Andrew Ryan <aryan17@mail.csuchico.edu>
2018-03-11 17:29:14 -0700

    starting search feature, update finished

--- webscraper/static/js/app.js
+++ webscraper/static/js/app.js
@@ -1,7 +1,7 @@
 $(document).foundation()
 
 document.getElementById("update").addEventListener("click", function(){
-    console.log("update button worked");
+    console.log("UPDATE button worked");
     document.getElementById("update").style.display = "none";
     document.getElementById("spinner").style.visibility = "visible";
 
@@ -9,5 +9,25 @@ document.getElementById("update").addEventListener("click", function(){
       url: '/runUpdate/',
     });
     setTimeout(function(){ window.location.reload() },6000)
+
+});
+
+document.getElementById("search").addEventListener("click", function(){
+   var category = document.getElementById("category").value;
+   var ownerAge = document.getElementById("ownerAge").value;
+   var equityValue = document.getElementById("equityValue").value;
+   var occupied = document.getElementById("occupied").value;
+   var lastSale = document.getElementById("lastSale").value;
+   console.log("SEARCH button worked");
+   console.log(category);
+   console.log(ownerAge);
+   console.log(equityValue);
+   console.log(occupied);
+   console.log(lastSale);
+
+    // $.ajax({
+    //   url: '/runUpdate/',
+    // });
+    // setTimeout(function(){ window.location.reload() },6000)
 
 });
